a21c2899032b94c6acf836be113a9a5569118006
jiiro974 <julien.rousselot@gmail.com>
2026-08-27 20:07:44 +0400

    release: paserial v0.3.13

--- Formula/paserial.rb
+++ Formula/paserial.rb
@@ -1,16 +1,16 @@
 class Paserial < Formula
   desc "PAN-OS serial console CLI — automatise les commandes PAN-OS via console série"
   homepage "https://github.com/jiiro974/paserial-release"
-  version "0.3.12"
+  version "0.3.13"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jiiro974/paserial-release/releases/download/v0.3.12/paserial-darwin-arm64"
-      sha256 "008f235c08a3914909b6baf4e58f908d57bb57af6a198dea0154573ee21bd644"
+      url "https://github.com/jiiro974/paserial-release/releases/download/v0.3.13/paserial-darwin-arm64"
+      sha256 "9f0f8b11ebc2dd343ffbd6705650bcd01d161a80e6bd05d05ac763c4ee625347"
     else
-      url "https://github.com/jiiro974/paserial-release/releases/download/v0.3.12/paserial-darwin-amd64"
-      sha256 "1629cef19b4f28fdf8341410c5d597ed6673e0a968362911958387a0769e7d98"
+      url "https://github.com/jiiro974/paserial-release/releases/download/v0.3.13/paserial-darwin-amd64"
+      sha256 "767b74d1d6ddaa829b126101ff92fa7064e77fcce65e46890b04926b1f2a32cb"
     end
   end
 
